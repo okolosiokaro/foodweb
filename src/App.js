c3cce1d7d4121fe6017050d6b63d2bb64309d590
Okolosiokaro <okolosiokaro@gmail.com>
2024-03-27 08:49:28 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import Hero from "./components/Hero";
+import HeadlineCards from "./components/HeadlineCards";
+import Food from "./components/Food";
+import Category from "./components/Category";
+
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar /> 
+    <Hero />
+    <HeadlineCards />
+    <Food />
+    <Category />
+    </div>
+  );
+}
+
+export default App;
