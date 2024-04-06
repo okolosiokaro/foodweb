6983182bad5daf751f9e104843acf47cc6d13085
Okolosiokaro <okolosiokaro@gmail.com>
2024-04-06 04:56:51 +0100

    staged changes

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -11,6 +11,8 @@ const [nav, setNav] = useState(false)
 const handleNav = () => {
     setNav(!nav)
 }
+nav ? document.body.classList.add('overflow-hidden') : document.body.classList.remove('overflow-hidden')
+
   return (
     <div className="max-w-[1640px] mx-auto flex justify-between items-center p-4">
       {/* left side */}
@@ -18,7 +20,7 @@ const handleNav = () => {
         <div onClick={handleNav} className='cursor-pointer'>
           <AiOutlineMenu  size={30}/>
         </div>
-        <h1 className='text-2xl sm:text-3xl lg:text-4xl'>Best <span className='font-bold'>Eats</span></h1>
+        <h1 className='text-2xl sm:text-3xl lg:text-4xl'>Buy <span className='font-bold'>Eats</span></h1>
         <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'> 
           <p className='bg-black rounded-full text-white p-2'>Delivery</p>
           <p className='p-2'>Pickup</p>
@@ -38,10 +40,10 @@ const handleNav = () => {
       {nav ? <div className='bg-black/70 fixed w-full h-screen z-10 top-0 left-0'></div> : ''}
       
       {/* side drawer menu */}
-      <div className={nav? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'}>
+      <div className={nav? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 ease-in-out duration-300' : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 ease-in-out duration-300'}>
         <AiOutlineClose size={30} onClick={handleNav} className='absolute right-4 top-4 cursor-pointer'/>
         <h2 className='text-2xl p-4'>
-          Best <span className='font-bold'>Eats</span>
+          Buy <span className='font-bold'>Eats</span>
         </h2>
         <nav>
           <ul className='flex flex-col p-4 text-gray-800'>
